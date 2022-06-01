9027b94a0220d91ae3d6ddc67507358d5bca20ed
linhao <823040382@qq.com>
2022-06-01 15:46:12 +0800

    添加池子合约信息

--- src/common/starblockdao-js/deployed.ts
+++ src/common/starblockdao-js/deployed.ts
@@ -9,7 +9,15 @@ export const deployed = {
   }
 };
 
-// const poolContract = {
-//   "StarBlock Genesis Pass (SGPass)":"0xe4895a9FEF7F186c848e9504b104C7ce3583f15a".tot
-
-// };
+const poolContract = {
+  "StarBlock Genesis Pass (SGPass)": "0xe4895a9FEF7F186c848e9504b104C7ce3583f15a".toLocaleLowerCase(),
+  "Bzuki (BZUKI)": "0x5d7181aEDAF8A900Cb8631F58CDeeD0CD7Bc936b".toLocaleLowerCase(),
+  "3Landers (3L)": "0x42f3FA68D37ef1925faa33A354913632191A1b8a".toLocaleLowerCase(),
+  "PLUR (PLUR)": "0xDAca7034ddD2262AEd2904eb52e4eb084FE48307".toLocaleLowerCase(),
+  "Dour Darcels (DARCEL)": "0xf7CB907251401616A1C1e34f08C22B155E320460".toLocaleLowerCase(),
+  "Rektguy (Rektguy)": "0x61DF7E1046c03Cf6f4df18879297895f7072CE3b".toLocaleLowerCase(),
+  "goblintown (GOBLIN)": "0x8E04E17B45F88e4e493740719bCF1eF8030554f4".toLocaleLowerCase(),
+  "DEGEN TOONZ (TOONZ)": "0x17561Ef085E9791af1C04c46EFBBbc355A1c33Cb".toLocaleLowerCase(),
+  "AIMoonbirds (MBAI)": "0xeED9BC3D71585d1DE2eA2D4A0f4664a6535Dd83c".toLocaleLowerCase(),
+  "Tasty Bones (TASTYBONES)": "0xa7162F138b5B02f46827e610FbD6A6ea8622fa68".toLocaleLowerCase()
+};
